014a722b158656ee697166d04a94786760888d6e
David <david_javier.torres@uao.edu.co>
2026-02-18 09:21:59 -0500

    Creacion de challenge

---
+++ 02-challenge/src/Loader.tsx
@@ -0,0 +1,40 @@
+import {useState, useEffect} from "react";
+
+function loadingLogo(){
+
+    const [load,setLoad] = useState!(true);
+    const [tiempo,setTiempo] = useState!(10);
+
+    useEffect!(() => {
+
+        if(tiempo > 0){
+            const intervalo = setTimeout(() =>  {
+
+                setTiempo(intervalo-1);
+
+            }, 1000);
+        
+            return() => clearTimeout(intervalo);
+        } else{
+
+            setLoad(false);
+
+        }
+
+    }, [tiempo]);
+
+    return(
+        
+        <div>
+
+            {load && <p>Cargando pagina.....</p>}
+            <img src= "https://tenor.com/es/view/loading-gif-6449096453315144907"
+            alt= "Imagen carga"></img>
+
+        </div>
+
+    );
+  
+}
+
+export default loadingLogo;
